4ae828d9e6ff1d78a52d8c8015a4409156e2a381
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-10 15:51:21 -0500

    Added skeleton for multiple pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage';
+import TestPage from './Pages/TestPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ROUTE_PATH = "Capstone";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/test" element={<TestPage/>} />
+          </Routes>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
