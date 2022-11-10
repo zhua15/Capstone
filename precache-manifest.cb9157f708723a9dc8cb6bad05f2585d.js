5c1882f2975fc88e104cbdf9b94d89a41f86be51
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-10 15:44:57 -0500

    Updates

---
+++ precache-manifest.cb9157f708723a9dc8cb6bad05f2585d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb7c5e24d65db21652a3",
+    "url": "/Capstone/static/css/main.b802e50e.chunk.css"
+  },
+  {
+    "revision": "eb7c5e24d65db21652a3",
+    "url": "/Capstone/static/js/main.eb7c5e24.chunk.js"
+  },
+  {
+    "revision": "a3266f79c2f28766ff68",
+    "url": "/Capstone/static/js/1.a3266f79.chunk.js"
+  },
+  {
+    "revision": "63b16ff37740cfeedbca",
+    "url": "/Capstone/static/js/runtime~main.63b16ff3.js"
+  },
+  {
+    "revision": "fc87c722faf3672c5f6e2d68c4d06209",
+    "url": "/Capstone/index.html"
+  }
+];
