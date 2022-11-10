b41ae25dc46e0c1e0b0ba96dbd507bcde6d827eb
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-10 15:52:33 -0500

    did stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 import TestPage from './Pages/TestPage';
 
-const ROUTE_PATH = "Capstone";
+const ROUTE_PATH = "/";
 
 class App extends Component {
   render() {
     return (
-      <HashRouter basename="/">
+      <HashRouter basename={ROUTE_PATH}>
         <div>
           <Routes>
             <Route path="/" element={<LandingPage/>} />
